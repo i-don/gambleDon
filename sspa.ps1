404986cf76339a6e2137737ebf7faabc6355186c
unknown <t-ishi@ba2.so-net.ne.jp>
2024-12-21 20:59:58 +0900

    power shell版追加

---
+++ sspa.ps1
@@ -0,0 +1,11 @@
+function check_python {
+	gcm python -ea SilentlyContinue | Out-Null
+	if ($? -eq $true) { # コマンドが存在すれば
+	    Write-Output 'Success!'
+	    python --version
+	} else {            # コマンドが存在しなければ
+	    Write-Error 'Error!'
+	    exit 1
+	}
+}
+check_python
